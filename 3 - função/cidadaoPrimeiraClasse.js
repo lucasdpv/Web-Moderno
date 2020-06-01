b7d2b6481db95c83b4df5337a37fbcdc4db185b9
Lucas Vieira <33061562+lucasdpv@users.noreply.github.com>
2020-06-01 16:17:50 -0300

    geral sobre funcao...

---
+++ 3 - função/cidadaoPrimeiraClasse.js
@@ -0,0 +1,28 @@
+// Função em JS é First-Class Object (Citizens)
+// Higher-order function
+
+// criar de forma literal
+function fun1() {}
+
+// Armazenar em uma variavel
+const fun2 = function () {};
+
+// Armazenar em um array
+const array = [
+  function (a, b) {
+    return a + b;
+  },
+  fun1,
+  fun2,
+];
+console.log(array[0](2,3));
+
+// Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function () { return 'opa'}
+console.log(obj.falar());
+
+// Passar função como parametro
+function run(fun){
+    fun()
+}
