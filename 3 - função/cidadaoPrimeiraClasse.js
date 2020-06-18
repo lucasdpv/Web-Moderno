94a6f5b818e512f915f10b110f121f2f94de6444
Lucas Vieira <lucasdpv@gmail.com>
2020-06-18 10:18:15 -0300

    estudos sobre funcoes

--- 3 - função/cidadaoPrimeiraClasse.js
+++ 3 - função/cidadaoPrimeiraClasse.js
@@ -15,14 +15,40 @@ const array = [
   fun1,
   fun2,
 ];
-console.log(array[0](2,3));
+console.log(array[0](2, 3));
 
 // Armazenar em um atributo de objeto
-const obj = {}
-obj.falar = function () { return 'opa'}
+const obj = {};
+obj.falar = function () {
+  return "opa";
+};
 console.log(obj.falar());
 
 // Passar função como parametro
-function run(fun){
-    fun()
+function run(fun) {
+  fun();
+}
+
+run(function () {
+  console.log("Executando...");
+});
+
+// Uma função pode retornar/conter um função
+function soma(a, b) {
+  return function (c) {
+    console.log(a + b + c);
+  };
+}
+
+
+var qualquer = [{id:'1'},{id:'2'},{id:'3'},{id:'4'}]
+
+var qualquer1 = [2,3,4,5]
+
+
+console.log(qualquer1.reduce(myFunc));
+
+
+function myFunc(total, num ) {
+   return total - num;
 }
